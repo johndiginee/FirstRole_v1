7efa360998166dce816fb731d4630888af25b5f9
John Diginee <Johndbizz@gmail.com>
2023-05-06 10:53:41 +0100

    Fix bugs

--- website/views.py
+++ website/views.py
@@ -9,3 +9,8 @@ def job_listing(request):
     jobs = Job.objects.filter(is_avaliable=True)
     context = {'jobs':jobs}
     return render(request, 'website/job-listing.html', context)
+
+def job_details(request, pk):
+    job = Job.objects.get(pk=pk)
+    context = {'job':job}
+    return render(request, 'website/job_details.html', context)

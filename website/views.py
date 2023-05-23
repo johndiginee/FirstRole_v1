260fa8cfd0e010976c4d280f61f30c0a25ddc542
John Diginee <Johndbizz@gmail.com>
2023-05-23 15:40:23 +0100

    Fix another bug

--- website/views.py
+++ website/views.py
@@ -19,5 +19,5 @@ def job_details(request, pk):
     else:
         has_applied = False
     job = Job.objects.get(pk=pk)
-    context = {'job':job}
+    context = {'job':job, 'has_applied':has_applied}
     return render(request, 'website/job_details.html', context)

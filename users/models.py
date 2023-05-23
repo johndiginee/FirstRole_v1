9b2fe11a99fd63500f35353a874496ed10eda70f
John Diginee <Johndbizz@gmail.com>
2023-05-23 09:46:11 +0100

    Add comments to User model

--- users/models.py
+++ users/models.py
@@ -2,6 +2,15 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    """Represent a User.
+    
+    Attributes:
+        email: a unique email address for the user
+        is_recruiter: a boolean field indicating whether the user is a recruiter
+        is_applicant: a boolean field indicating whether the user is an applicant
+        has_resume: a boolean field indicating whether the user has a resume
+        has_company: a boolean field indicating whether the user has a company.
+    """
     email = models.EmailField(unique=True)
     is_recruiter = models.BooleanField(default=False)
     is_applicant = models.BooleanField(default=False)

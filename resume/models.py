cc1a014bbdce48281eb3f613df836c9d1b99bc01
John Diginee <Johndbizz@gmail.com>
2023-05-23 09:45:32 +0100

    Add comments to Resume model

--- resume/models.py
+++ resume/models.py
@@ -2,6 +2,16 @@ from django.db import models
 from users.models import User
 
 class Resume(models.Model):
+    """Represent a Resume.
+    
+    Attributes:
+        user: A OneToOneField that links the resume to a user.
+        first_name: A CharField to store the applicant's first name.
+        surname: A CharField to store the applicant's surname.
+        location: A CharField to store the applicant's location.
+        job_title: A CharField to store the applicant's job title.
+        upload_resume: A FileField to upload the applicant's resume/cv.
+    """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=100, null=True, blank=True)
     surname = models.CharField(max_length=100, null=True, blank=True)

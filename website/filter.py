839e3b1eded236c0f656bb215dafde6ae7d1caa4
John Diginee <Johndbizz@gmail.com>
2023-05-24 15:20:54 +0100

    Add ModelChoiceFilter in Django Filters for Dropdown-Based Filtering

--- website/filter.py
+++ website/filter.py
@@ -1,9 +1,23 @@
 import django_filters
-from job.models import Job
+from job.models import Job, State, Industry
+from django import forms
 
 
 class Jobfilter(django_filters.FilterSet):
     title = django_filters.CharFilter(lookup_expr='icontains')
+    state = django_filters.ModelChoiceFilter(
+        queryset=State.objects.all(),
+        empty_label="All States",
+        label="States",
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        )
+    industry = django_filters.ModelChoiceFilter(
+        queryset=State.objects.all(),
+        empty_label="All Industries",
+        label="Industries",
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        )
+
     class Meta:
         model = Job
         fields = ['title', 'state', 'job_type', 'industry']

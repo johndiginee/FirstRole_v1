dc01a5d952d06b2d44c586a260e6bfc1b799137a
John Diginee <Johndbizz@gmail.com>
2023-05-24 16:25:05 +0100

    Implementing pagination

--- website/views.py
+++ website/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from job.models import Job, ApplyJob
 from .filter import Jobfilter
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 def home(request):
     """Filter internships on home page."""
@@ -12,7 +14,20 @@ def home(request):
 def job_listing(request):
     """List avaliable jobs/internships."""
     jobs = Job.objects.filter(is_avaliable=True).order_by('-timestamp')
-    context = {'jobs':jobs}
+
+    #Paginatio
+    page_num = request.GET.get('page', 1)
+    paginator = Paginator(jobs, 6) # 6 employees per page
+    try:
+        page_obj = paginator.page(page_num)
+    except PageNotAnInteger:
+        # if page is not an integer, deliver the first page
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        # if the page is out of range, deliver the last page
+        page_obj = paginator.page(paginator.num_pages)
+
+    context = {'jobs':jobs, 'page_obj': page_obj}
     return render(request, 'website/job-listing.html', context)
 
 def job_details(request, pk):

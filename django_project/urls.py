134646c7e925f9aa15148815146bf87402c2201e
John Diginee <Johndbizz@gmail.com>
2023-05-24 10:28:45 +0100

    Done adding 404 page

--- django_project/urls.py
+++ django_project/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import handler404, handler500
+
 
 urlpatterns = [
     path('', include('website.urls')),

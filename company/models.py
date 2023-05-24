dcc88e769ab555b41a6684f353c42d5673a00935
John Diginee <Johndbizz@gmail.com>
2023-05-24 08:27:46 +0100

    Add website field to company info

--- company/models.py
+++ company/models.py
@@ -14,6 +14,7 @@ class Company(models.Model):
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100, null=True, blank=True)
+    website = models.CharField(max_length=100, null=True, blank=True)
     est_date = models.PositiveIntegerField(null=True, blank=True)
     city = models.CharField(max_length=100, null=True, blank=True)
     state = models.CharField(max_length=100, null=True, blank=True)
